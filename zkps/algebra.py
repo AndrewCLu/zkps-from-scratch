7edb98d2325df8202854e723c028453bfedc5db5
AndrewCLu <andrewclu98@gmail.com>
2023-02-25 18:09:18 -0500

    implement scalar mul and div for polynomials:

--- zkps/algebra.py
+++ zkps/algebra.py
@@ -2,7 +2,8 @@ from py_ecc import bn128, bls12_381
 from py_ecc.fields.field_elements import FQ
 from py_ecc.typing import Field
 from dataclasses import dataclass
-from typing import TypeVar, Generic, List, Type
+from typing import TypeVar, Generic, List, Type, Union, Tuple
+from copy import deepcopy
 
 class bn128_FR(FQ):
     field_modulus = bn128.curve_order
@@ -39,6 +40,7 @@ class Polynomial(Generic[FElt]):
         
         return res
 
+    # TODO: Auto truncate zero leading coeffs in poly
     def __add__(self, other: 'Polynomial') -> 'Polynomial':
         longer, shorter = self.coeffs, other.coeffs
         if len(self.coeffs) < len(other.coeffs):
@@ -53,25 +55,52 @@ class Polynomial(Generic[FElt]):
         return Polynomial[FElt](new_coeffs)
 
     # TODO: FFT
-    def __mul__(self, other: 'Polynomial') -> 'Polynomial':
+    def __mul__(self, other: Union[FElt, 'Polynomial']) -> 'Polynomial':
         new_coeffs: List[FElt] = []
-        for i in range(len(self.coeffs)):
-            for j in range(len(other.coeffs)):
-                index = i + j
-                prod = self.coeffs[i] * self.coeffs[j]
-                if index >= len(new_coeffs):
-                    new_coeffs.append(prod)
-                else:
-                    new_coeffs[index] += prod
+        if isinstance(other, FElt):
+            for i in range(len(self.coeffs)):
+                new_coeffs.append(other * self.coeffs[i])
+        elif isinstance(other, Polynomial):
+            for i in range(len(self.coeffs)):
+                for j in range(len(other.coeffs)):
+                    index = i + j
+                    prod = self.coeffs[i] * self.coeffs[j]
+                    if index >= len(new_coeffs):
+                        new_coeffs.append(prod)
+                    else:
+                        new_coeffs[index] += prod
+        else:
+            raise Exception('Argument to mul must be a field element or polynomial!')
         
         return Polynomial[FElt](new_coeffs)
 
-    def __truediv__(self, other: FElt) -> 'Polynomial':
-        new_coeffs: List[FElt] = []
-        for i in range(len(self.coeffs)):
-            new_coeffs.append(self.coeffs[i] / other)
-        
-        return Polynomial[FElt](new_coeffs)
+    # Returns (quotient, remainder) after division by other
+    def __truediv__(self, other: Union[FElt, 'Polynomial']) -> Tuple['Polynomial', 'Polynomial']:
+        if isinstance(other, FElt):
+            new_coeffs: List[FElt] = []
+            for i in range(len(self.coeffs)):
+                new_coeffs.append(self.coeffs[i] / other)
+            return (Polynomial[FElt](new_coeffs), Polynomial[FElt]([self.coeffs[0].zero()])) # TODO: Fix these class method calls
+        elif isinstance(other, Polynomial):
+            num_coeffs = deepcopy(self.coeffs)
+            den_coeffs = deepcopy(other.coeffs)
+            if len(num_coeffs) < len(den_coeffs):
+                return (Polynomial[FElt]([self.coeffs[0].zero()]), Polynomial[FElt](num_coeffs)) # TODO: Fix these class method calls
+            quo_coeffs: List[FElt] = []
+            zero: FElt = self.coeffs[0].zero() # TODO: Fix these class method calls
+            while len(num_coeffs) >= len(den_coeffs):
+                if num_coeffs[-1] == zero:
+                    quo_coeffs.insert(0, self.coeffs[0].zero()) # TODO: Fix these class method calls
+                    num_coeffs.pop()
+                else:
+                    quo = num_coeffs[-1] / den_coeffs[-1]
+                    quo_coeffs.insert(0, quo)
+                    for i in range(len(quo_coeffs)):
+                        num_coeffs[-(1+i)] -= quo * den_coeffs[-(1+i)]
+                    num_coeffs.pop()
+            return (Polynomial[FElt](quo_coeffs), Polynomial[FElt](num_coeffs))
+        else:
+            raise Exception('Argument to div must be a field element or polynomial!')
 
     # TODO: IFFT
     @staticmethod
@@ -94,7 +123,8 @@ class Polynomial(Generic[FElt]):
         res = Polynomial[FElt]([field_class.one()])
         for i in range(len(domain)):
             if i != index:
-                res *= Polynomial[FElt]([-domain[i], field_class.one()]) / (domain[index] - domain[i])
+                quo, _ = Polynomial[FElt]([-domain[i], field_class.one()]) / (domain[index] - domain[i])
+                res *= quo
         
         return res
         
