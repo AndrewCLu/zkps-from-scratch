0a2de8456a8056fe3b752101406a0021cee90894
AndrewCLu <andrewclu98@gmail.com>
2023-03-09 19:30:52 -0700

    add batch open at single point capabiltiy to pcs

--- zkps/polynomial_commitment_schemes/pcs.py
+++ zkps/polynomial_commitment_schemes/pcs.py
@@ -1,7 +1,7 @@
 from typing import Generic, Any
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
-from algebra import FElt, Polynomial
+from algebra import FElt, Polynomial, List
 from utils import Byteable
 
 @dataclass
@@ -21,7 +21,15 @@ class PCSProver(ABC, Generic[FElt]):
     def open(self, f: Polynomial[FElt], cm: Commitment, z: FElt, s: FElt, op_info: Any) -> Opening:
         pass
 
+    @abstractmethod
+    def batch_open_at_point(self, fs: List[Polynomial[FElt]], cms: List[Commitment], z: FElt, ss: List[FElt], op_info: Any) -> Opening:
+        pass
+
 class PCSVerifier(ABC, Generic[FElt]):
     @abstractmethod
     def verify_opening(self, op: Opening, cm: Commitment, z: FElt, s: FElt, op_info: Any) -> bool:
+        pass
+
+    @abstractmethod
+    def verify_batch_at_point(self, op: Opening, cms: List[Commitment], z: FElt, ss: List[FElt], op_info: Any) -> bool:
         pass
--- zkps/polynomial_commitment_schemes/trivial.py
+++ zkps/polynomial_commitment_schemes/trivial.py
@@ -19,7 +19,7 @@ class TrivialCommitment(Commitment, Byteable, Generic[FElt]):
 
     def __str__(self) -> str:
         return ", ".join([str(x) for x in self.value])
-    
+
 @dataclass
 class TrivialOpening(Opening):
     value: None
@@ -33,10 +33,37 @@ class TrivialProver(PCSProver, Generic[FElt]):
             raise Exception('Wrong commitment used. Must provide a trivial commitment.')
         return TrivialOpening(value=None)
 
+    def batch_open_at_point(self, fs: List[Polynomial[FElt]], cms: List[Commitment], z: FElt, ss: List[FElt], op_info: Any) -> Opening:
+        batch_size = len(fs)
+        if len(cms) != batch_size or len(ss) != batch_size:
+            raise Exception('All parameters must have length equal to batch size!')
+        
+        return TrivialOpening(value=None)
+
 class TrivialVerifier(PCSVerifier, Generic[FElt]):
     def verify_opening(self, op: Opening, cm: Commitment, z: FElt, s: FElt, op_info: Any) -> bool:
+        if not isinstance(op, TrivialOpening):
+            raise Exception('Wrong opening used. Must provide a trivial opening.')
         if not isinstance(cm, TrivialCommitment):
             raise Exception('Wrong commitment used. Must provide a trivial commitment.')
         coeffs: List[FElt] = cast(TrivialCommitment[FElt], cm).value
         f = Polynomial[FElt](coeffs=coeffs)
         return f(z) == s
+    
+    def verify_batch_at_point(self, op: Opening, cms: List[Commitment], z: FElt, ss: List[FElt], op_info: Any) -> bool:
+        if not isinstance(op, TrivialOpening):
+            raise Exception('Wrong opening used. Must provide a trivial opening.')
+        
+        batch_size = len(cms)
+        if len(ss) != batch_size:
+            raise Exception('All parameters must have length equal to batch size!')
+
+        for i in range(batch_size):
+            if not isinstance(cms[i], TrivialCommitment):
+                raise Exception('Wrong commitment used. Must provide a trivial commitment.')
+        
+        for i in range(batch_size):
+            if not self.verify_opening(op=op, cm=cms[i], z=z, s=ss[i], op_info=op_info):
+                return False
+        
+        return True
